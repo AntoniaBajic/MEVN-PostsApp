2617c01ebb1a75d70e9f5bed3838331786866727
Antonia Bajic <antoniaa.bajic@gmail.com>
2020-06-25 23:54:45 +0200

    get  post

--- client/src/components/PostComponent.vue
+++ client/src/components/PostComponent.vue
@@ -1,30 +1,79 @@
 <template>
-  <div class="hello"></div>
+  <div class="container">
+    <h1>Newest posts</h1>
+    <p class="error" v-if="error">{{ error }}</p>
+    <div class="posts-container">
+      <div
+        class="post"
+        v-for="(post, index) in posts"
+        v-bind:item="post"
+        v-bind:index="index"
+        v-bind:key="post._id"
+      >
+        {{
+          `${post.createdAt.getDate()}/${post.createdAt.getMonth()}/${post.createdAt.getFullYear()}`
+        }}
+        <p class="text">{{ post.text }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import PostService from '../PostService';
 export default {
-  name: "PostComponent",
-  props: {
-    msg: String
-  }
+  name: 'PostComponent',
+  data() {
+    return {
+      posts: [],
+      error: '',
+      text: '',
+    };
+  },
+  async created() {
+    try {
+      this.posts = await PostService.getPosts();
+    } catch (err) {
+      this.error = err.message;
+    }
+  },
 };
 </script>
 
-
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+div.container {
+  max-width: 800px;
+  margin: 0 auto;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+p.error {
+  border: 1px solid #ff5b5f;
+  background-color: #ffc5c1;
+  padding: 10px;
+  margin-bottom: 15px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+div.post {
+  position: relative;
+  border: 1px solid #8858d6;
+  background-color: #b19bd3;
+  padding: 10px 10px 30px 10px;
+  margin-bottom: 15px;
 }
-a {
-  color: #42b983;
+
+div.created-at {
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 5px 15px 5px 15px;
+  background-color: #6f34ce;
+  color: white;
+  font-size: 13px;
+}
+
+p.text {
+  font-size: 22px;
+  font-weight: 700;
+  margin-bottom: 0;
 }
 </style>
